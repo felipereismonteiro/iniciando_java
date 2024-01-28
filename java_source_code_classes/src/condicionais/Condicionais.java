301d52ed8dc6df0bd00854145c85ec0d8cfb8d07
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-28 12:26:30 -0300

    feat: added switch case

--- java_source_code_classes/src/condicionais/Condicionais.java
+++ java_source_code_classes/src/condicionais/Condicionais.java
@@ -18,4 +18,19 @@ public class Condicionais {
    System.out.println("Filme retro que vale a pena assistir");
   }
  }
+
+//  switch (expressão) {
+//   case valor1:
+//      // código a ser executado se a expressão for igual a valor1
+//      break;
+//   case valor2:
+//      // código a ser executado se a expressão for igual a valor2
+//      break;
+//   case valor3:
+//      // código a ser executado se a expressão for igual a valor3
+//      break;
+//   default:
+//      // código a ser executado se a expressão não for igual a nenhum valor
+//      break;
+// }
 }
