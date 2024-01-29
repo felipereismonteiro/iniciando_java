bddebed3426bb0a0f11346f54cc8c4a1b4f67d7d
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-29 19:21:01 -0300

    Desafio metodos

--- java_source_code_classes/src/desafio_conta/CriaConta.java
+++ java_source_code_classes/src/desafio_conta/CriaConta.java
@@ -6,16 +6,26 @@ public class CriaConta {
         Conta primeraConta = new Conta();
         primeraConta.saldo = 200;
 
-        System.out.println(primeraConta.saldo);
+        // System.out.println(primeraConta.saldo);
         primeraConta.saldo += 100;
 
-        System.out.println(primeraConta.saldo);
+        // System.out.println(primeraConta.saldo);
 
         Conta segundaConta = primeraConta;
 
         segundaConta.saldo = 50;
 
-        System.out.println("Primeira conta tem o saldo de: " + primeraConta.saldo);
-        System.out.println("Segunda conta tem o saldo de: " + segundaConta.saldo);
+        // System.out.println("Primeira conta tem o saldo de: " + primeraConta.saldo);
+        // System.out.println("Segunda conta tem o saldo de: " + segundaConta.saldo);
+
+        Conta contaFelipe = new Conta();
+        Conta contaGabriel = new Conta();
+
+        contaFelipe.deposita(100);
+        contaFelipe.transfere(100, contaGabriel);
+
+        System.out.println(contaGabriel.saldo);
+        System.out.println(contaFelipe.saldo);
+
     }
 }
