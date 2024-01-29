49e5fd2967dc140dbd83b76e3a9b97acaab527de
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-29 18:50:46 -0300

    Desafio da conta

---
+++ java_source_code_classes/src/desafio_conta/Conta.java
@@ -0,0 +1,8 @@
+package java_source_code_classes.src.desafio_conta;
+
+public class Conta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+}
---
+++ java_source_code_classes/src/desafio_conta/CriaConta.java
@@ -0,0 +1,21 @@
+package java_source_code_classes.src.desafio_conta;
+
+public class CriaConta {
+    public static void main(String[] args) {
+
+        Conta primeraConta = new Conta();
+        primeraConta.saldo = 200;
+
+        System.out.println(primeraConta.saldo);
+        primeraConta.saldo += 100;
+
+        System.out.println(primeraConta.saldo);
+
+        Conta segundaConta = primeraConta;
+
+        segundaConta.saldo = 50;
+
+        System.out.println("Primeira conta tem o saldo de: " + primeraConta.saldo);
+        System.out.println("Segunda conta tem o saldo de: " + segundaConta.saldo);
+    }
+}
