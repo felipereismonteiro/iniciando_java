285e6d0a25be12874584461eb87db42a4c8df2b2
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2024-01-29 20:40:22 -0300

    Orientação a objetos

--- java_source_code_classes/src/desafio_conta/Conta.java
+++ java_source_code_classes/src/desafio_conta/Conta.java
@@ -5,6 +5,27 @@ public class Conta {
     private int agencia;
     private int numero;
     private Cliente titular;
+    private static int totalContas;
+    /*
+    * static é tipo uma variavel global
+    * ele pertence a clase entao sempre que cria uma nova classe ele usa a variavel
+    * que estava antes e como esta ++ no construtor ele simplesmente adiciona
+    * mais um a variave global
+    * */
+    public Conta(int agencia, int numero) {
+        Conta.totalContas++;
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = 100; //isso significa que toda conta começa com 100 de saldo.
+        System.out.println("Estou criando uma conta");
+    }
+
+    public static int getTotalContas() {
+        // No caso a gente pode usar o totalContas mesmo que seja privado
+        // e em um metodo estatico nao existe um this pq com ele instancia
+        // a conta nao existe tipo um essa conta entende
+        return Conta.totalContas;
+    }
 
     public void setTitular(Cliente titular) {
         this.titular = titular;
--- java_source_code_classes/src/desafio_conta/CriaConta.java
+++ java_source_code_classes/src/desafio_conta/CriaConta.java
@@ -3,7 +3,7 @@ package java_source_code_classes.src.desafio_conta;
 public class CriaConta {
     public static void main(String[] args) {
 
-        Conta primeraConta = new Conta();
+        Conta primeraConta = new Conta(123, 123);
         primeraConta.depositar(200);
 
         // System.out.println(primeraConta.saldo);
@@ -18,8 +18,8 @@ public class CriaConta {
         // System.out.println("Primeira conta tem o saldo de: " + primeraConta.saldo);
         // System.out.println("Segunda conta tem o saldo de: " + segundaConta.saldo);
 
-        Conta contaFelipe = new Conta();
-        Conta contaGabriel = new Conta();
+        Conta contaFelipe = new Conta(123, 123);
+        Conta contaGabriel = new Conta(123 ,132);
 
         contaFelipe.depositar(100);
         contaFelipe.transfere(100, contaGabriel);
@@ -29,5 +29,8 @@ public class CriaConta {
         contaFelipe.getTitular().setNome("Felipe");
 
         System.out.println(contaFelipe.getTitular().getNome());
+
+        // Precisamos pegar a instancia conta para acessar seu metodo estatico
+        System.out.println(Conta.getTotalContas());
     }
 }
